940cc50758a4df5554e00a90e67c34fcc9862b66
Destynee Martine <deermartinez8@gmail.com>
2021-02-04 18:44:29 -0700

    bootstraps adds

--- my-app/src/Components/bootstrap.js
+++ my-app/src/Components/bootstrap.js
@@ -11,3 +11,82 @@ function bootstrap(){
 
 
 export default bootstrap;
+
+
+import React, { useState } from 'react';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Button color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Toggle</Button>
+      <Collapse isOpen={isOpen}>
+        <Card>
+          <CardBody>
+          Anim pariatur cliche reprehenderit,
+           enim eiusmod high life accusamus terry richardson ad squid. Nihil
+           anim keffiyeh helvetica, craft beer labore wes anderson cred
+           nesciunt sapiente ea proident.
+          </CardBody>
+        </Card>
+      </Collapse>
+    </div>
+  );
+}
+
+export default Example;
+
+
+import React from 'react';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
+const Example = (props) => {
+  return (
+    <Pagination aria-label="Page navigation example">
+    <PaginationItem>
+        <PaginationLink first href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink previous href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          1
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          2
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          3
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          4
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink href="#">
+          5
+        </PaginationLink>
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink next href="#" />
+      </PaginationItem>
+      <PaginationItem>
+        <PaginationLink last href="#" />
+      </PaginationItem>
+    </Pagination>
+  );
+}
+
+export default Example;
+
